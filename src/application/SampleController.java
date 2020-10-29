4c4fad0d95d109e3d482fa8e6db7e1262e993e53
karolkiljan <karolkiljan@gmail.om>
2020-10-29 02:19:21 +0100

    Pierwsza wersja

---
+++ src/application/SampleController.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class SampleController {
+
+	@FXML TextArea textArea;
+
+	@FXML public void about(ActionEvent event) {
+		textArea.textProperty().set("You clicked on 'About'");	
+		
+	}
+	
+}
